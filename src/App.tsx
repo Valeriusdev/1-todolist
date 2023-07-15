5cfa89a28de9aa11c5d6c088235d9682f6b348ff
Valeriusdev <kakao757@gmail.com>
2023-07-14 20:37:41 -0700

    update tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,18 @@ import './App.css';
 import { Todolist } from './Todolist';
 
 function App() {
-
-
-    const truck = "hi"
-    const truckNew = "hello"
-
-    const tasks1 = [
+    
+    const tasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
-    ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "Redux", isDone: false }
     ]
 
     return (
-        <>
-            <Todolist truck={truck} truck2={11} tasks={tasks1} />
-            <Todolist truck={truckNew} truck3={true} tasks={tasks2} />
-        </>
+        <div className="App">
+            <Todolist title='What to learn' tasks={tasks} />           
+        </div>
     );
 }
 
