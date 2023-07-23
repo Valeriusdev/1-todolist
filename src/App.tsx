4521be71b5fbf4fe144c8e2ee09ed7196aa803a2
Valeriusdev <kakao757@gmail.com>
2023-07-22 23:55:32 -0700

    add removeTask function

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Todolist } from './Todolist';
 
 function App() {
     
-    const tasks = [
+    const [tasks, setTasks] = useState([
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
         { id: 4, title: "Redux", isDone: false }
-    ]
+    ]);
+
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks);
+    }
+
+    
 
     return (
         <div className="App">
-            <Todolist title='What to learn' tasks={tasks} />           
+            <Todolist title='What to learn'
+                      tasks={tasks}
+                      removeTask = {removeTask} 
+        />           
         </div>
     );
 }
